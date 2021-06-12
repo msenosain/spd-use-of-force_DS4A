806d50b0782c71efe6ea443d73346089ccd79dcc
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2021-06-12 11:41:47 -0500

    added Rudy's and Melodie's

--- src/data/01_data-cleaning.py
+++ src/data/01_data-cleaning.py
@@ -3,67 +3,82 @@ import pandas as pd
 import numpy as np
 import os
 
-# Create a directory for processed data
-os.mkdir('cleaned_files')
+# data directories
+raw = 'raw'
+processed = 'processed'
+
+# datasets filenames
+train_uof = 'Miriam VonAschen-Cook - UseOfForceProject-UoF Data w requested fields.xlsx'
+cases_pattern = 'Miriam VonAschen-Cook - CAD20'
+crisis = 'Miriam VonAschen-Cook - Crisis Report Preliminary Data.xlsx'
+crime = 'Miriam VonAschen-Cook - Crime_Data.csv'
 
 ##############################################################################
 # Training/Use of Force data
 ##############################################################################
 # Read
-df = pd.DataFrame(pd.read_excel('Miriam VonAschen-Cook - UseOfForceProject-UoF Data w requested fields.xlsx'))
+df = pd.DataFrame(pd.read_excel(os.path.join(raw, train_uof))
 
 # Clean
 df.loc[(df['Subject Gender'] == "Male"), "Subject Gender"] = "M"
 df.loc[(df['Subject Gender'] == "Female"), "Subject Gender"] = "F"
 df.loc[(df['Subject Gender'] == "Not Specified"), "Subject Gender"] = "N"
+## Removing leading/trailling spaces from strings
 df.loc[:,df.dtypes == object] = df.loc[:,df.dtypes == object].apply(lambda x: x.str.strip().str.replace('#NAME?', '-'))
 
 # Write
-df.to_csv('cleaned_files/MVA-train_cleaned.csv', index = False)
+df.to_csv(os.path.join(processed, 'MVAtrain_cleaned.csv'), index = False)
+
 
 ##############################################################################
 # Cases data
 ##############################################################################
-# Read
-
-# Concatenate files
+# Read and Concatenate files
 df = pd.DataFrame()
 
-cases_fn = os.listdir()
+cases_fn = os.listdir(raw)
 
-for file in os.listdir():
-    if file.startswith("Miriam VonAschen-Cook - CAD20"):
-        f = pd.read_csv(file)
+for file in cases_fn:
+    if file.startswith(cases_pattern):
+        f = pd.read_csv(os.path.join(raw,file))
+        f['Year'] = int(re.findall("\d+", file)[0])
         df = df.append(f, ignor_index = True)
 
 # Clean
+## Removing leading/trailling spaces from strings
 df.loc[:,df.dtypes == object] = df.loc[:,df.dtypes == object].apply(lambda x: x.str.strip().str.replace('#NAME?', '-'))
+## Clean “CAD Event ID” by converting it to integer
+df['CAD Event ID'] = df['CAD Event ID'].astype(int)
+## Clean “Officer Serial Num” by converting it to integer
+df['Officer Serial Num'] = df['Officer Serial Num'].astype(int)
 
 # Write
-df.to_csv('cleaned_files/MVA-allcases_cleaned.csv', index = False)
+df.to_csv(os.path.join(processed, 'MVAallcases_cleaned.csv'), index = False)
+
 
 ##############################################################################
 # Crime data
 ##############################################################################
 # Read
-
+df = pd.read_csv()
 # Clean
 df.loc[:,df.dtypes == object] = df.loc[:,df.dtypes == object].apply(lambda x: x.str.strip().str.replace('#NAME?', '-'))
 
 # Write
+df.to_csv(os.path.join(processed, 'MVAcrime_cleaned.csv'), index = False)
 
 
 ##############################################################################
 # Crisis data
 ##############################################################################
 # Read
-df = pd.DataFrame(pd.read_excel('Miriam VonAschen-Cook - Crisis Report Preliminary Data.xlsx'))
-
+df = pd.DataFrame(pd.read_excel(crisis))
 
 # Clean
+## Removing leading/trailling spaces from strings
 df.loc[:,df.dtypes == object] = df.loc[:,df.dtypes == object].apply(lambda x: x.str.strip().str.replace('#NAME?', '-'))
 
 # Write
-df.to_csv('cleaned_files/MVA-crisis_cleaned.csv', index = False)
+df.to_csv(os.path.join(processed, 'MVAcrisis_cleaned.csv'), index = False)
 
 
