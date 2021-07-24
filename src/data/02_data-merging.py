4da7fcf0f3944dd32233e702dde1f7c616ef1de7
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2021-07-24 16:29:32 -0500

    fixing merge

--- src/data/02_data-merging.py
+++ src/data/02_data-merging.py
@@ -23,15 +23,31 @@ def merge_datasets():
     # Drop "CAD Event ID" = -1 from crisis dataset
     df_crisis.drop(df_crisis[df_crisis["CAD Event ID"] == -1].index, inplace=True)
 
+    # drop columns in crisis
+    df_crisis.drop(['OffenseIncident'], axis = 1, inplace=True)
+    # drop columns in cases
+    df_cases.drop(['Officer Dispatch UID', 'Dispatch ID', 'AS Of Officer Title',
+        'As_On_Officer Precinct_ID', 'As_On_Officer Precinct_Desc', 
+        'As_On_Officer Squad_Desc', 'Partner Officer Serial Num', 
+        'Clear By Desc', 'Call Type Desc','Case Type Initial Desc', 
+        'Case Type Final Desc','Clear Time','Dispatch Address'], 
+        axis = 1, inplace=True)
+    # drop columns in crime
+    df_crime.drop(['Offense ID', 'Offense Start DateTime', 'Report DateTime', 
+        'NIBRS Offense Code', 'Precinct', 'Sector', 'Beat', 'MCPP', 
+        '100 Block Address', 'Longitude', 'Latitude', 'Year'], 
+        axis = 1, inplace=True)
+    # drop columns in uof
+    df_uof.drop(['File Num', 'Longitude', 'Latitude', 
+        'Officer Title (as of Incident)','Precinct', 'Sector','Subject Gender', 
+        'Subject Race', 'Occurred Date'], axis = 1, inplace=True)
+
     # left merge cases to crime dataset
-    df_merged = pd.merge(df_cases, df_crime, how = "left", on = ["GO Num"], 
-        suffixes = ('_calls', '_crime'))
+    df_merged = pd.merge(df_cases, df_crime, how = "left", on = ["GO Num"]
     # left merge cases+crime to train/uof dataset
-    df_merged = pd.merge(df_merged, df_uof, how = "left", on = ["GO Num"], 
-        suffixes = ('_crime', '_train'))
+    df_merged = pd.merge(df_merged, df_uof, how = "left", on = ["GO Num", "Officer Serial Num"])
     # left merge crisis to cases+crime+train/uof datasets
-    df_merged = pd.merge(df_crisis, df_merged, how = "left", on = ["CAD Event ID"],
-        suffixes = ('_crisis', '_train'))
+    df_merged = pd.merge(df_crisis, df_merged, how = "left", on = ["CAD Event ID"])
 
     # Write csv
     df_merged.to_csv(os.path.join(filepath, 'MVA_cleaned_merged.csv'), index = False)
